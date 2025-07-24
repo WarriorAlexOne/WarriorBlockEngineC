98354cfada861c4871bc8ea39b2ecd7fb1bea02d
WarriorAlexOne <warrioralexone@gmail.com>
2025-07-23 20:21:28 -0500

    Test Commit

--- src/main.c
+++ src/main.c
@@ -1,15 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-#include "Utils/WBE_String.h"
-#include "Utils/WBE_Math.h"
-#include "WBE_Clock.h"
 #include "Tests/Test_WBE_Math.h"
 
 int main (int argc, char **argv) {
     WBE_TestMathFunctions();
 
-    printf("Am I Looping? %lli\n", WBE_Fibonacci(6));
+    // printf("Am I Looping?\n");
     return 0;
 }
