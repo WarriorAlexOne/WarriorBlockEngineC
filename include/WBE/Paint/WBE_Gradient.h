509ca55942c4aff25e1917335448c2047239f2d3
WarriorAlexOne <warrioralexone@gmail.com>
2026-02-15 13:18:53 -0600

    Added 'padding' and 'alpha' parameters to Gradient function.

--- include/WBE/Paint/WBE_Gradient.h
+++ include/WBE/Paint/WBE_Gradient.h
@@ -4,6 +4,6 @@
 #include <SDL3/SDL.h>
 #include "WBE/WBE_Defines.h"
 
-SDL_Texture* WBE_CreateGradient (SDL_Renderer* renderer, ubyte r1, ubyte g1, ubyte b1, ubyte r2, ubyte g2, ubyte b2);
+SDL_Texture* WBE_CreateGradient (SDL_Renderer* renderer, unsigned int padding, ubyte alpha, ubyte r1, ubyte g1, ubyte b1, ubyte r2, ubyte g2, ubyte b2);
 
 #endif
--- src/Paint/WBE_Gradient.c
+++ src/Paint/WBE_Gradient.c
@@ -1,6 +1,8 @@
 #include "WBE/Paint/WBE_Gradient.h"
 
-SDL_Texture* WBE_CreateGradient (SDL_Renderer* renderer, ubyte r1, ubyte g1, ubyte b1, ubyte r2, ubyte g2, ubyte b2) {
+SDL_Texture* WBE_CreateGradient (SDL_Renderer* renderer, unsigned int padding, ubyte alpha, ubyte r1, ubyte g1, ubyte b1, ubyte r2, ubyte g2, ubyte b2) {
+    if (padding % 2 != 0) padding++;
+    
     float r_diff = r1 > r2 ? r1 - r2 : r2 - r1;
     float g_diff = g1 > g2 ? g1 - g2 : g2 - g1;
     float b_diff = b1 > b2 ? b1 - b2 : b2 - b1;
@@ -19,9 +21,17 @@ SDL_Texture* WBE_CreateGradient (SDL_Renderer* renderer, ubyte r1, ubyte g1, uby
     float g = g1;
     float b = b1;
 
-    SDL_Surface* newGradient = SDL_CreateSurface(biggestDiff+1, 1, SDL_PIXELFORMAT_RGBA8888);
-    for (int i = 0; i <= biggestDiff+1; i++) {
-        SDL_WriteSurfacePixel(newGradient, i, 0, (int)r, (int)g, (int)b, 255);
+    SDL_Surface* newGradient = SDL_CreateSurface(biggestDiff+1+(padding*2), 1, SDL_PIXELFORMAT_RGBA8888);
+    for (int i = 0; i < padding; i++) {
+        SDL_WriteSurfacePixel(newGradient, i, 0, (int)r, (int)g, (int)b, alpha);
+    }
+    for (int i = padding; i <= biggestDiff+padding; i++) {
+        SDL_WriteSurfacePixel(newGradient, i, 0, (int)r, (int)g, (int)b, alpha);
+        if (i == (biggestDiff+padding)) {
+            for (int j = biggestDiff+1+padding; j < biggestDiff+1+(padding*2); j++) {
+                SDL_WriteSurfacePixel(newGradient, j, 0, (int)r, (int)g, (int)b, alpha);
+            }
+        }
 
         r += r1 > r2 ? -r_steps : r_steps;
         g += g1 > g2 ? -g_steps : g_steps;
