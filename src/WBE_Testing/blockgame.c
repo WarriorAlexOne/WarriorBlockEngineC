8a812282dafbc5da00a827419250e00648498fc0
WarriorAlexOne <warrioralexone@gmail.com>
2026-03-17 21:44:27 -0500

    BlockGame Test: Optimized cube rendering with face culling, memory optimizations, and meshing.

--- src/WBE_Testing/blockgame.c
+++ src/WBE_Testing/blockgame.c
@@ -9,7 +9,7 @@
 // #define WORLDY 16
 // #define WORLDZ 16
 
-#define WORLD_SIZE 64
+#define WORLD_SIZE 92
 #define BLOCK_SIZE 1.0f
 
 int*** blocks = NULL;
@@ -17,6 +17,8 @@ int*** blocks = NULL;
 Mesh worldMesh = {0};
 Texture2D blockTexture = {0};
 Material blockMaterial = {0};
+
+bool cullFaces = true;
 
 bool worldIsBuilt = false;
 
@@ -35,14 +37,6 @@ void MyMesh () {
     }
     int cubeVerts = 18;
     int cubeTexUVCoords = 12;
-    int cubeSides = 6;
-    int numberOfCubes = WORLD_SIZE*WORLD_SIZE*WORLD_SIZE;
-
-    worldMesh.vertices = SDL_malloc((((sizeof(float) * cubeVerts)*cubeSides)*numberOfCubes)*6);
-    worldMesh.texcoords = SDL_malloc(((sizeof(float) * cubeTexUVCoords)*cubeSides)*numberOfCubes);
-
-    worldMesh.vertexCount = (cubeSides*numberOfCubes)*6;
-    worldMesh.triangleCount = (2*numberOfCubes);
 
     // Stored cube data
     float quad1[18] = {0, 0, 0,  0, 1, 0,  1, 1, 0,    1, 1, 0,  1, 0, 0,  0, 0, 0};
@@ -64,114 +58,171 @@ void MyMesh () {
     float quad6Tex[6] = {1, 1,  0, 1,  0, 0};
 
 
+    bool firstRun = true;
+    int allFaceCount = 0;
     int vertCount = 0;
     int texCount = 0;
-    for (int x = 0; x < WORLD_SIZE; x++) {
-        for (int y = 0; y < WORLD_SIZE; y++) {
-            for (int z = 0; z < WORLD_SIZE; z++) {
-                if ((z == 0 && CheckBlock(blocks, 1, x, y, z)) || (CheckBlock(blocks, 1, x, y, z) && CheckBlock(blocks, 0, x, y, z-1 >= 0 ? z-1 : z))) {
-                    for (int verts = 0; verts < 6; verts++) { // Face 1
-                        worldMesh.vertices[vertCount++] = quad1[verts*3]+x;
-                        worldMesh.vertices[vertCount++] = quad1[(verts*3)+1]+y;
-                        worldMesh.vertices[vertCount++] = quad1[(verts*3)+2]+z;
+    for (int run = 0; run < 2; run++) {
+        // SDL_Log("Running!");
+        if (!firstRun) {
+            SDL_Log("Face Count: %i", allFaceCount);
+            worldMesh.vertices = SDL_malloc(((sizeof(float) * cubeVerts) * allFaceCount));
+            worldMesh.texcoords = SDL_malloc(((sizeof(float) * cubeTexUVCoords) * allFaceCount));
+
+            worldMesh.vertexCount = (6 * allFaceCount);
+            worldMesh.triangleCount = (2 * allFaceCount);
+        }
+        for (int x = 0; x < WORLD_SIZE; x++) {
+            for (int y = 0; y < WORLD_SIZE; y++) {
+                for (int z = 0; z < WORLD_SIZE; z++) {
+                    // X+ Face
+                    if ((z == 0 && CheckBlock(blocks, 1, x, y, z)) || (CheckBlock(blocks, 1, x, y, z) && CheckBlock(blocks, 0, x, y, z-1 >= 0 ? z-1 : z))) {
+                        if (firstRun) {
+                            allFaceCount++;
+                            goto face1Checked;
+                        }
+                        // SDL_Log("First Face!");
+                        for (int verts = 0; verts < 6; verts++) { // Face 1
+                            worldMesh.vertices[vertCount++] = quad1[verts*3]+x;
+                            worldMesh.vertices[vertCount++] = quad1[(verts*3)+1]+y;
+                            worldMesh.vertices[vertCount++] = quad1[(verts*3)+2]+z;
+                        }
+
+                        for (int verts = 0; verts < 3; verts++) { // Face 1
+                            worldMesh.texcoords[texCount++] = quad1Tex[verts*2];
+                            worldMesh.texcoords[texCount++] = quad1Tex[(verts*2)+1];
+                        }
+                        for (int verts = 0; verts < 3; verts++) { // Face 1
+                            worldMesh.texcoords[texCount++] = -quad1Tex[verts*2];
+                            worldMesh.texcoords[texCount++] = -quad1Tex[(verts*2)+1];
+                        }
                     }
 
-                    for (int verts = 0; verts < 3; verts++) { // Face 1
-                        worldMesh.texcoords[texCount++] = quad1Tex[verts*2];
-                        worldMesh.texcoords[texCount++] = quad1Tex[(verts*2)+1];
-                    }
-                    for (int verts = 0; verts < 3; verts++) { // Face 1
-                        worldMesh.texcoords[texCount++] = -quad1Tex[verts*2];
-                        worldMesh.texcoords[texCount++] = -quad1Tex[(verts*2)+1];
-                    }
-                }
-
-                if ((x == 0 && CheckBlock(blocks, 1, x, y, z)) || (CheckBlock(blocks, 1, x, y, z) && CheckBlock(blocks, 0, x-1 >= 0 ? x-1 : x, y, z))) {
-                    for (int verts = 0; verts < 6; verts++) { // Face 2
-                        worldMesh.vertices[vertCount++] = quad2[verts*3]+x;
-                        worldMesh.vertices[vertCount++] = quad2[(verts*3)+1]+y;
-                        worldMesh.vertices[vertCount++] = quad2[(verts*3)+2]+z;
-                    }
-                    for (int verts = 0; verts < 3; verts++) { // Face 2
-                        worldMesh.texcoords[texCount++] = quad2Tex[verts*2];
-                        worldMesh.texcoords[texCount++] = quad2Tex[(verts*2)+1];
-                    }
-                    for (int verts = 0; verts < 3; verts++) { // Face 2
-                        worldMesh.texcoords[texCount++] = -quad2Tex[verts*2];
-                        worldMesh.texcoords[texCount++] = -quad2Tex[(verts*2)+1];
-                    }
-                }
-
-                if ((z == WORLD_SIZE-1 && CheckBlock(blocks, 1, x, y, z)) || (CheckBlock(blocks, 1, x, y, z) && CheckBlock(blocks, 0, x, y, z+1 < WORLD_SIZE ? z+1 : z))) {
-                    for (int verts = 0; verts < 6; verts++) { // Face 3
-                        worldMesh.vertices[vertCount++] = quad3[verts*3]+x;
-                        worldMesh.vertices[vertCount++] = quad3[(verts*3)+1]+y;
-                        worldMesh.vertices[vertCount++] = quad3[(verts*3)+2]+z;
+                    face1Checked:
+                    // Z+ Face
+                    if ((x == 0 && CheckBlock(blocks, 1, x, y, z)) || (CheckBlock(blocks, 1, x, y, z) && CheckBlock(blocks, 0, x-1 >= 0 ? x-1 : x, y, z))) {
+                        if (firstRun) {
+                            allFaceCount++;
+                            goto face2Checked;
+                        }
+                        // SDL_Log("Second Face!");
+                        for (int verts = 0; verts < 6; verts++) { // Face 2
+                            worldMesh.vertices[vertCount++] = quad2[verts*3]+x;
+                            worldMesh.vertices[vertCount++] = quad2[(verts*3)+1]+y;
+                            worldMesh.vertices[vertCount++] = quad2[(verts*3)+2]+z;
+                        }
+                        for (int verts = 0; verts < 3; verts++) { // Face 2
+                            worldMesh.texcoords[texCount++] = quad2Tex[verts*2];
+                            worldMesh.texcoords[texCount++] = quad2Tex[(verts*2)+1];
+                        }
+                        for (int verts = 0; verts < 3; verts++) { // Face 2
+                            worldMesh.texcoords[texCount++] = -quad2Tex[verts*2];
+                            worldMesh.texcoords[texCount++] = -quad2Tex[(verts*2)+1];
+                        }
                     }
 
-                    for (int verts = 0; verts < 3; verts++) { // Face 3
-                        worldMesh.texcoords[texCount++] = quad3Tex[verts*2];
-                        worldMesh.texcoords[texCount++] = quad3Tex[(verts*2)+1];
-                    }
-                    for (int verts = 0; verts < 3; verts++) { // Face 3
-                        worldMesh.texcoords[texCount++] = -quad3Tex[verts*2];
-                        worldMesh.texcoords[texCount++] = -quad3Tex[(verts*2)+1];
-                    }
-                }
+                    face2Checked:
+                    // X- Face
+                    if ((z == WORLD_SIZE-1 && CheckBlock(blocks, 1, x, y, z)) || (CheckBlock(blocks, 1, x, y, z) && CheckBlock(blocks, 0, x, y, z+1 < WORLD_SIZE ? z+1 : z))) {
+                        if (firstRun) {
+                            allFaceCount++;
+                            goto face3Checked;
+                        }
+                        // SDL_Log("Third Face!");
+                        for (int verts = 0; verts < 6; verts++) { // Face 3
+                            worldMesh.vertices[vertCount++] = quad3[verts*3]+x;
+                            worldMesh.vertices[vertCount++] = quad3[(verts*3)+1]+y;
+                            worldMesh.vertices[vertCount++] = quad3[(verts*3)+2]+z;
+                        }
 
-                if ((x == WORLD_SIZE-1 && CheckBlock(blocks, 1, x, y, z)) || (CheckBlock(blocks, 1, x, y, z) && CheckBlock(blocks, 0, x+1 < WORLD_SIZE ? x+1 : x, y, z))) {
-                    for (int verts = 0; verts < 6; verts++) { // Face 4
-                        worldMesh.vertices[vertCount++] = quad4[verts*3]+x;
-                        worldMesh.vertices[vertCount++] = quad4[(verts*3)+1]+y;
-                        worldMesh.vertices[vertCount++] = quad4[(verts*3)+2]+z;
-                    }
-
-                    for (int verts = 0; verts < 3; verts++) { // Face 4
-                        worldMesh.texcoords[texCount++] = quad4Tex[verts*2];
-                        worldMesh.texcoords[texCount++] = quad4Tex[(verts*2)+1];
-                    }
-                    for (int verts = 0; verts < 3; verts++) { // Face 4
-                        worldMesh.texcoords[texCount++] = -quad4Tex[verts*2];
-                        worldMesh.texcoords[texCount++] = -quad4Tex[(verts*2)+1];
-                    }
-                }
-
-                if ((y == WORLD_SIZE-1 && CheckBlock(blocks, 1, x, y, z)) || (CheckBlock(blocks, 1, x, y, z) && CheckBlock(blocks, 0, x, y+1 < WORLD_SIZE ? y+1 : y, z))) {
-                    for (int verts = 0; verts < 6; verts++) { // Face 5
-                        worldMesh.vertices[vertCount++] = quad5[verts*3]+x;
-                        worldMesh.vertices[vertCount++] = quad5[(verts*3)+1]+y;
-                        worldMesh.vertices[vertCount++] = quad5[(verts*3)+2]+z;
+                        for (int verts = 0; verts < 3; verts++) { // Face 3
+                            worldMesh.texcoords[texCount++] = quad3Tex[verts*2];
+                            worldMesh.texcoords[texCount++] = quad3Tex[(verts*2)+1];
+                        }
+                        for (int verts = 0; verts < 3; verts++) { // Face 3
+                            worldMesh.texcoords[texCount++] = -quad3Tex[verts*2];
+                            worldMesh.texcoords[texCount++] = -quad3Tex[(verts*2)+1];
+                        }
                     }
 
-                    for (int verts = 0; verts < 3; verts++) { // Face 5
-                        worldMesh.texcoords[texCount++] = quad5Tex[verts*2];
-                        worldMesh.texcoords[texCount++] = quad5Tex[(verts*2)+1];
-                    }
-                    for (int verts = 0; verts < 3; verts++) { // Face 5
-                        worldMesh.texcoords[texCount++] = -quad5Tex[verts*2];
-                        worldMesh.texcoords[texCount++] = -quad5Tex[(verts*2)+1];
-                    }
-                }
+                    face3Checked:
+                    // Z- Face
+                    if ((x == WORLD_SIZE-1 && CheckBlock(blocks, 1, x, y, z)) || (CheckBlock(blocks, 1, x, y, z) && CheckBlock(blocks, 0, x+1 < WORLD_SIZE ? x+1 : x, y, z))) {
+                        if (firstRun) {
+                            allFaceCount++;
+                            goto face4Checked;
+                        }
+                        // SDL_Log("Forth Face!");
+                        for (int verts = 0; verts < 6; verts++) { // Face 4
+                            worldMesh.vertices[vertCount++] = quad4[verts*3]+x;
+                            worldMesh.vertices[vertCount++] = quad4[(verts*3)+1]+y;
+                            worldMesh.vertices[vertCount++] = quad4[(verts*3)+2]+z;
+                        }
 
-                if ((y == 0 && CheckBlock(blocks, 1, x, y, z)) || (CheckBlock(blocks, 1, x, y, z) && CheckBlock(blocks, 0, x, y-1 >= 0 ? y-1 : y, z))) {
-                    for (int verts = 0; verts < 6; verts++) { // Face 6
-                        worldMesh.vertices[vertCount++] = quad6[verts*3]+x;
-                        worldMesh.vertices[vertCount++] = quad6[(verts*3)+1]+y;
-                        worldMesh.vertices[vertCount++] = quad6[(verts*3)+2]+z;
+                        for (int verts = 0; verts < 3; verts++) { // Face 4
+                            worldMesh.texcoords[texCount++] = quad4Tex[verts*2];
+                            worldMesh.texcoords[texCount++] = quad4Tex[(verts*2)+1];
+                        }
+                        for (int verts = 0; verts < 3; verts++) { // Face 4
+                            worldMesh.texcoords[texCount++] = -quad4Tex[verts*2];
+                            worldMesh.texcoords[texCount++] = -quad4Tex[(verts*2)+1];
+                        }
                     }
 
-                    for (int verts = 0; verts < 3; verts++) { // Face 6
-                        worldMesh.texcoords[texCount++] = quad6Tex[verts*2];
-                        worldMesh.texcoords[texCount++] = quad6Tex[(verts*2)+1];
+                    face4Checked:
+                    // Y+ Face
+                    if ((y == WORLD_SIZE-1 && CheckBlock(blocks, 1, x, y, z)) || (CheckBlock(blocks, 1, x, y, z) && CheckBlock(blocks, 0, x, y+1 < WORLD_SIZE ? y+1 : y, z))) {
+                        if (firstRun) {
+                            allFaceCount++;
+                            goto face5Checked;
+                        }
+                        // SDL_Log("Fifth Face!");
+                        for (int verts = 0; verts < 6; verts++) { // Face 5
+                            worldMesh.vertices[vertCount++] = quad5[verts*3]+x;
+                            worldMesh.vertices[vertCount++] = quad5[(verts*3)+1]+y;
+                            worldMesh.vertices[vertCount++] = quad5[(verts*3)+2]+z;
+                        }
+
+                        for (int verts = 0; verts < 3; verts++) { // Face 5
+                            worldMesh.texcoords[texCount++] = quad5Tex[verts*2];
+                            worldMesh.texcoords[texCount++] = quad5Tex[(verts*2)+1];
+                        }
+                        for (int verts = 0; verts < 3; verts++) { // Face 5
+                            worldMesh.texcoords[texCount++] = -quad5Tex[verts*2];
+                            worldMesh.texcoords[texCount++] = -quad5Tex[(verts*2)+1];
+                        }
                     }
-                    for (int verts = 0; verts < 3; verts++) { // Face 6
-                        worldMesh.texcoords[texCount++] = -quad6Tex[verts*2];
-                        worldMesh.texcoords[texCount++] = -quad6Tex[(verts*2)+1];
+
+                    face5Checked:
+                    // Y- Face
+                    if ((y == 0 && CheckBlock(blocks, 1, x, y, z)) || (CheckBlock(blocks, 1, x, y, z) && CheckBlock(blocks, 0, x, y-1 >= 0 ? y-1 : y, z))) {
+                        if (firstRun) {
+                            allFaceCount++;
+                            goto face6Checked;
+                        }
+                        // SDL_Log("Sixth Face!");
+                        for (int verts = 0; verts < 6; verts++) { // Face 6
+                            worldMesh.vertices[vertCount++] = quad6[verts*3]+x;
+                            worldMesh.vertices[vertCount++] = quad6[(verts*3)+1]+y;
+                            worldMesh.vertices[vertCount++] = quad6[(verts*3)+2]+z;
+                        }
+
+                        for (int verts = 0; verts < 3; verts++) { // Face 6
+                            worldMesh.texcoords[texCount++] = quad6Tex[verts*2];
+                            worldMesh.texcoords[texCount++] = quad6Tex[(verts*2)+1];
+                        }
+                        for (int verts = 0; verts < 3; verts++) { // Face 6
+                            worldMesh.texcoords[texCount++] = -quad6Tex[verts*2];
+                            worldMesh.texcoords[texCount++] = -quad6Tex[(verts*2)+1];
+                        }
                     }
+                    face6Checked:
                 }
             }
         }
+        firstRun = false;
     }
+    // SDL_Log("Done!");
 
     UploadMesh(&worldMesh, false);
     worldIsBuilt = true;
@@ -256,8 +307,8 @@ int main () {
                     if (blocks[x][y][z] == 1) {
                         Vector3 pos = {(float)x, (float)y, (float)z};
                         BoundingBox box = {
-                            {pos.x - 0.5f, pos.y - 0.5f, pos.z - 0.5f},
-                            {pos.x + 0.5f, pos.y + 0.5f, pos.z + 0.5f}
+                            {pos.x, pos.y, pos.z},
+                            {pos.x + 1.0f, pos.y + 1.0f, pos.z + 1.0f}
                         };
                         RayCollision hit = GetRayCollisionBox(mouseRay, box);
                         if (hit.hit && hit.distance < minDistance) {
@@ -286,12 +337,40 @@ int main () {
 
                     // If block ID is 1, block is solid
                     if (blocks[x][y][z] == 1) {
-                        Vector3 pos = {(float)x+0.5f, (float)y+0.5f, (float)z+0.5f};
+                        Vector3 pos = {(float)x, (float)y, (float)z+0.5f};
 
                         // If player is facing a cube, color it red
                         if (hasHit && x == closestX && y == closestY && z == closestZ) {
-                            // DrawModel(cubeModel, pos, 1.0f, RED);
-                            // DrawCube(pos, 1.1f, 1.1f, 1.1f, RED);
+                            float outlineSize = 0.01f;
+
+                            pos.x += 1;
+                            DrawCube(pos, outlineSize, outlineSize, 1.0f+outlineSize, BLACK);
+                            pos.y += 1;
+                            DrawCube(pos, outlineSize, outlineSize, 1.0f+outlineSize, BLACK);
+                            pos.x -= 1;
+                            DrawCube(pos, outlineSize, outlineSize, 1.0f+outlineSize, BLACK);
+                            pos.y -= 1;
+                            DrawCube(pos, outlineSize, outlineSize, 1.0f+outlineSize, BLACK);
+                            
+                            pos.x += 0.5f;
+                            pos.z += 0.5f;
+                            DrawCube(pos, 1.0f+outlineSize, outlineSize, outlineSize, BLACK);
+                            pos.y += 1;
+                            DrawCube(pos, 1.0f+outlineSize, outlineSize, outlineSize, BLACK);
+                            pos.z -= 1;
+                            DrawCube(pos, 1.0f+outlineSize, outlineSize, outlineSize, BLACK);
+                            pos.y -= 1;
+                            DrawCube(pos, 1.0f+outlineSize, outlineSize, outlineSize, BLACK);
+
+                            pos.x += 0.5f;
+                            pos.y += 0.5f;
+                            DrawCube(pos, outlineSize, 1.0f+outlineSize, outlineSize, BLACK);
+                            pos.x -= 1;
+                            DrawCube(pos, outlineSize, 1.0f+outlineSize, outlineSize, BLACK);
+                            pos.z += 1;
+                            DrawCube(pos, outlineSize, 1.0f+outlineSize, outlineSize, BLACK);
+                            pos.x += 1;
+                            DrawCube(pos, outlineSize, 1.0f+outlineSize, outlineSize, BLACK);
                         }
                     }
                 }
@@ -319,6 +398,7 @@ int main () {
         DrawFPS(10, 10);
         DrawText(TextFormat("Coords: (%.1f, %.1f, %.1f)", camera.position.x, camera.position.y, camera.position.z), 10, 30, 20, LIGHTGRAY);
         DrawText("Titanium Block Texture By Dedede2!", 10, 50, 20, LIGHTGRAY);
+        DrawText(TextFormat("World Size: %i", WORLD_SIZE), 10, 70, 20, LIGHTGRAY);
         
         EndDrawing();
 
