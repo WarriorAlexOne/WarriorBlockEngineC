2cc4b5fedd438bbc3040bd45d5b23f876a01ecda
WarriorAlexOne <warrioralexone@gmail.com>
2026-06-28 05:26:08 -0500

    Extra cleanup code I forgot to commit.

--- src/Input/WBE_Input.c
+++ src/Input/WBE_Input.c
@@ -1,14 +1,23 @@
 #include "WBE/Input/WBE_Input.h"
 #include <SDL3/SDL.h>
+#include "WBE/WBE_Instance.h"
+#include "WBE/WBE_Window.h"
+#include "WBE/WBE_Clock.h"
 
 
 void WBE_UpdateInput (WBE_Instance* instance) {
     while (SDL_PollEvent(&instance->event)) {
         if (instance->event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED) {
-            SDL_DestroyWindow(SDL_GetWindowFromID(instance->event.window.windowID));
+            for (int i = 0; i < instance->windowCount; i++) {
+                WBE_Window* getWindow = instance->windows[i];
+                if (getWindow->sdl_id == instance->event.window.windowID) {
+                    getWindow->quitWindow = true;
+                }
+                CloseWindow(instance);
+            }
         }
         if (instance->event.type == SDL_EVENT_QUIT) {
-            instance->quitProgram = true;
+            WBE_QuitInstance(instance);
         }
         WBE_UpdateKeys(instance);
     }
